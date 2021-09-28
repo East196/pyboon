d493a7088df915b343facd90dbd6c7e41302a7fa
east196 <2901180515@qq.com>
2021-09-28 22:56:27 +0800

    + more test

--- test/test_x.py
+++ test/test_x.py
@@ -1,4 +1,8 @@
 from pyboon.x import *
 
-def test_to_camel():
-    assert 'helloNewWorld' == to_camel("HelloNew_world")
+def test_to_xxx():
+    assert 'hello new world' == to_seq("HelloNew_world")
+    assert 'HelloNewWorld'   == to_pascal("HelloNew_world")
+    assert 'helloNewWorld'   == to_camel("HelloNew_world")
+    assert 'hello_new_world' == to_underscore("HelloNew_world")
+    assert 'hello-new-world' == to_minus("HelloNew_world")
