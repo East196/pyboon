d495a395e746040a624adaf31b0d8b5439a9c5dd
east196 <2901180515@qq.com>
2021-10-01 14:54:57 +0800

    + sql qb

--- pyboon/sql.py
+++ pyboon/sql.py
@@ -26,6 +26,23 @@ def qb(l,sql):
     print(result)
     return result
 
+def qb_one(l,sql):
+    ret = re.search(r"SELECT (.*) FROM", sql)
+    _, v = ret.group(0), ret.group(1)
+    fields = v.split(",")
+    print(fields)
+    result = []
+    for item in l:
+        n = {}
+        for field in fields:
+            if field in item:
+                n[field] = item[field]
+            else:
+                print(field,"not in item")
+        result.append(n)
+    print(result)
+    return result[0] if result else None
+
 
 if __name__ == '__main__':
     qb([{"a": 1, "b": 2, "c": 3, "d": 4, }],"SELECT a,b FROM tab")
--- test/test_sql.py
+++ test/test_sql.py
@@ -6,3 +6,4 @@ def test_sql():
 
 def test_qb():
     assert [{'a': 1, 'b': 2}] == qb([{"a":1,"b":2,"c":3,"d":4,}],"SELECT a,b,e FROM tab")
+    assert {'a': 1, 'b': 2} == qb_one([{"a":1,"b":2,"c":3,"d":4,}],"SELECT a,b,e FROM tab")
